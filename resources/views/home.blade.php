448dbd8ef99bf5434623e3e37ea722812d4d6ba3
josstell10 <josstell@gmail.com>
2021-02-21 17:29:47 -0500

    inicio

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no" name="viewport">
+</head>
+<body>
+	<h1>Hola {{ $name }}</h1>
+</body>
+</html>
